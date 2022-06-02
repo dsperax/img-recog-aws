26abd50c6ac86bcc65463a00a11bb78829558ec6
dsperax <dsperax@gmail.com>
2022-06-02 14:44:27 -0300

    Using dynamoDb

---
+++ trigger-dynamo/src/util/decoratorValidator.js
@@ -0,0 +1,23 @@
+const decoratorValidator = (fn, schema, argsType) => {
+    return async function (event) {
+        const data = JSON.parse(event[argsType])
+        //abortEarly == mostrar todos os erros de uma só vez
+        const { error, value } = await schema.validate(
+            data, { abortEarly: true }
+        )
+
+        //altera a instancia dos argumentos
+        event[argsType] = value
+        //arguments serve para pegar todos os argumentos que vieram
+        //e mandar para frente
+        //o apply vai retornar a função que será executada posteriormente
+        if (!error) return fn.apply(this, arguments)
+
+        return {
+            statusCode: 422, //unprocessable entity
+            body: error.mensage
+        }
+    }
+}
+
+module.exports = decoratorValidator
